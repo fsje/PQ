7f2cebd5eeb0d5958b3e6cf15bf9fc3c994fa13c
Frederik Eriksen <fsje@live.dk>
2021-01-04 20:25:09 +0100

    Updated bootstrap JS.

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -13,6 +13,6 @@
         
         <!-- Popper.JS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-        <script src="https://premiumquality.dk/assets/bootstrap/js/bootstrap.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
         <script src="https://premiumquality.dk/views/js/common.js"></script>
