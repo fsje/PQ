6a97de1dc6c764745334f54f2589f2161e236c8e
Frederik Eriksen <fsje@live.dk>
2021-02-19 16:27:42 +0100

    Cleanup

--- app/login.php
+++ app/login.php
@@ -10,9 +10,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $accountNumber = $_POST['accountNumber'];
     $password      = $_POST['password'];
 
-    #echo $accountNumber . ' ' . password_hash($password, PASSWORD_BCRYPT);
-
-
     if(isset($accountNumber) && isset($password))
     {
         $user = $user->authUser($accountNumber, $password);
